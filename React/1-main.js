f6f15ea50399a696526b0eecdd5168c23d4b5cf1
Adebisola <adeniyiadebisola@gmail.com>
2024-03-05 20:35:41 +0100

    update

--- React/1-main.js
+++ React/1-main.js
@@ -4,5 +4,19 @@
 
 
 /* arrow functions*/
-let greeting = (message) => alert(`${message} everyone!`);
+/*let greeting = message => alert(`${message} everyone!`);
 greeting("Buenas tardes");
+let greeting = () => alert(`Hello everyone!`);*/ // no argument
+
+
+let createBlog = (title, body) => {
+    if (!title){
+        throw new Error("A title is required");
+    }
+    if (!body) {
+        throw new Error("Body cannot be empty");
+    }
+    return alert (`${title} - ${body}`);
+
+};
+createBlog("Blog title", "Blog body")
