d50d57fa3270ceedd21d0fe17732d65b1421300c
Adebisola <adeniyiadebisola@gmail.com>
2024-03-13 20:55:17 +0100

    learning

--- sass_scss/4-main.js
+++ sass_scss/4-main.js
@@ -16,3 +16,56 @@ const JSX = (
   );
   // Add your code below this line
   ReactDOM.render(JSX, document.getElementById("challenge-node"));
+
+
+
+  // difference between JSX and HTML camelCase use of closing tags
+
+
+  const DemoComponent = function() {
+    return (
+      <div className='customClass' />
+    );
+  };
+
+  // stateless components only renders and does not manage nor track changes to the data.
+
+  const MyComponent = function() {
+    return (
+      <div className = "myClass">
+        <p> THis is an example of a stateless component</p>
+      </div>
+    )
+  }
+
+  // ES6 class component
+  class Kitten extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+  
+    render() {
+      return (
+        <h1>Hi</h1>
+      );
+    }
+  }
+
+  class MyComponent1 extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      // Change code below this line
+      return(
+        <div>
+          <h1>Hello React!</h1>
+         </div>
+      );
+  
+  
+      // Change code above this line
+    }
+  };
+
+  //Create a Component with Composition:mutltiple react components.
