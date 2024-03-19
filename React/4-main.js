db68bbcf03d72fb0b0fdbc44b9e40c1f8396f436
Adebisola <adeniyiadebisola@gmail.com>
2024-03-19 17:03:29 +0100

    learn

--- React/4-main.js
+++ React/4-main.js
@@ -198,3 +198,148 @@ class TypesOfFood extends React.Component {
     );
   }
 };
+
+
+ 
+ const sample = (<div id="id01">
+  Hello World!
+ ReactDOM.render(
+    <h1>Hello React!</h1>,
+    document.getElementById('id01'));
+ </div>
+ )
+
+ function Welcome() {
+  return <h1>Hello React!</h1>;
+}
+ReactDOM.render(<Welcome />, document.getElementById('root'));
+
+function Welcome(props) {
+  return <h1>Hello {props.name}!</h1>;
+}
+ReactDOM.render(<Welcome name="John Doe"/>, document.getElementById('root'));
+
+
+class Welcome extends React.Component {
+  render() { return(<h1>Hello {this.props.name}</h1>); }
+}
+ReactDOM.render(<Welcome name="John Doe"/>, document.getElementById('root'));
+
+
+
+
+// rendering class components to the DOM
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        {/* Change code below this line */}
+        <Fruits />
+        <Vegetables />
+
+        {/* Change code above this line */}
+      </div>
+    );
+  }
+};
+
+// Change code below this line
+ReactDOM.render(<TypesOfFood />, document.getElementById("challenge-node"));
+
+
+// my first react component
+// Change code below this line
+class MyComponent_ extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return(
+      <div>
+        <h1> My First React Component!</h1>
+      </div>
+    )
+  } 
+}
+ReactDOM.render(<MyComponent_ />, document.getElementById("challenge-node"));
+
+
+//Pass Props to a Stateless Functional Component
+
+<App>
+  <Welcome user='Mark' />
+</App>
+const Welcome = (props) => <h1>Hello, {props.user}!</h1>
+{/*It is standard to call this value props and when dealing with stateless functional components, you basically consider it as an argument to a function which returns JSX. You can access the value of the argument in the function body. */}
+
+const CurrentDate = (props) => {
+  return (
+    <div>
+      { /* Change code below this line */ }
+      
+      <p>The current date is:{props.date} </p>
+      < Date  />
+      { /* Change code above this line */ }
+    </div>
+  );
+};
+
+class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>What date is it?</h3>
+        { /* Change code below this line */ }
+        <CurrentDate date= {Date()} />
+        { /* Change code above this line */ }
+      </div>
+    );
+  }
+};
+
+
+//Pass an Array as Props
+
+<ParentComponent>
+  <ChildComponent_ colors={["green", "blue", "red"]} />
+</ParentComponent>
+
+// accessing the property in the child component
+const ChildComponent_ = (props) => <p>{props.colors.join(', ')}</p>
+
+const List = (props) => {
+  { /* Change code below this line */ }
+  return <p>{props.tasks.join(" , ")}</p>
+  { /* Change code above this line */ }
+};
+
+class ToDo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>To Do Lists</h1>
+        <h2>Today</h2>
+        { /* Change code below this line */ }
+
+
+        <List tasks={["Walk the dog", "feed the fish", "clean the room."]} />
+        <h2>Tomorrow</h2>
+        <List tasks={["see the client","cook the meat","Wash the kitchen."]} />
+        { /* Change code above this line */ }
+      </div>
+    );
+  }
+};
+
+
+//Use Default Props
---
+++ my-react-app/src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to my React App !</h1>
+          <p> This is an example of a stateless component</p>
+          <div class="list">
+            <p>Lists of Fruits</p>
+            <ul>
+              <li>Mango</li>
+              <li>Bananas</li>
+              <li>Orange</li>
+              <li>Watermelon</li>
+            </ul>
+          </div>    
+          
+    </div>
+  );
+}
+
+export default App;
+
+ 
